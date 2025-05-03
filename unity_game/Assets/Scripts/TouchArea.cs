4ccff12350434a8b3c1344f266609b478b3ad664
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-05-04 02:52:29 +0530

    Added the TouchArea.cs script to handle the touch movements to control the player paddle.

--- unity_game/Assets/Scripts/TouchArea.cs
+++ unity_game/Assets/Scripts/TouchArea.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class TouchArea : MonoBehaviour
+public class TouchArea : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Transform playerPaddle;
+
     void Start()
     {
-        
+        GameObject paddleObj = GameObject.FindGameObjectWithTag("Player");
+        if (paddleObj != null)
+        {
+            playerPaddle = paddleObj.transform;
+        }
+        else
+        {
+            Debug.LogError("Player paddle with tag 'Player' not found.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        MovePaddle(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        MovePaddle(eventData);
+    }
+
+    private void MovePaddle(PointerEventData eventData)
+    {
+        if (playerPaddle == null) return;
+
+        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(eventData.position);
+        Vector2 paddlePos = playerPaddle.position;
+        playerPaddle.position = new Vector2(worldPoint.x, paddlePos.y);
     }
 }
